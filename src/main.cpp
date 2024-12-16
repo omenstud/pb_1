acab0ee3bb4e7d15f9f3e6f5386bbcde7027736b
Visel Nikita <nikitavisel@gmail.com>
2024-12-16 10:39:32 +0300

    [lab-3] World 1

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,57 @@
 
 #include "karel.h"
 
+bool shouldTurnRight;
+
+void turnRight() {
+    TurnLeft();
+    TurnLeft();
+    TurnLeft();
+}
 
 //==============================================================================
 /// Программа для робота Карела
 ///
 void KarelProgram() {
 /// @todo Добавить программу для Карела
+while (true) {
+    int counter = 0;
 
+    while (BeepersPresent()) {
+        PickBeeper();
+        counter++;
+    }
+
+    for (int i = 0; i < counter * 2; i++) {
+        PutBeeper();
+    }
+
+    if (FrontIsClear()) {
+        Move();
+    } else if (shouldTurnRight) {
+        turnRight();
+        
+        if (FrontIsClear()) {
+            Move();
+        } else {
+            Finish();
+        }
+
+        turnRight();
+        shouldTurnRight = false;
+    } else {
+        TurnLeft();
+
+        if (FrontIsClear()) {
+            Move();
+        } else {
+            Finish();
+        }
+
+        TurnLeft();
+        shouldTurnRight = true;
+    }
+}
 }
 
 
@@ -22,7 +66,7 @@ void KarelProgram() {
 /// Стартовая функция
 ///
 int main() {
-    LoadWorld("worlds/.w");
+    LoadWorld("worlds/Double_beepers_01.w");
     KarelProgram();
     Finish();
     return 0;
